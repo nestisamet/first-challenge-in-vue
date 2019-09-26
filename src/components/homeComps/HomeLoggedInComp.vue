ad7e29930098d599b04c9337ff6b8b07a983029f
Samet <nestisamet@gmail.com>
2019-09-26 15:38:27 +0300

    initial commit

---
+++ src/components/homeComps/HomeLoggedInComp.vue
@@ -0,0 +1,38 @@
+<template>
+         <b-card
+                  title="LOGGED IN"
+                  tag="article"
+                  style="max-width: 40rem;"
+                  class="mb-6 text-center display-flex"
+                  >
+                    <hr />
+                    <b-card-text>
+                    Samet's Homework
+                    </b-card-text>
+                     <b-card-text>
+                                <slot></slot>
+                    </b-card-text>
+                    
+                  
+        </b-card>
+</template>
+<script>
+
+export default {
+    name:'HomeLoggedInComp',
+    data(){
+        return{
+            data:[]
+        }
+    },
+    components:{
+
+    },
+    methods:{
+       
+    }
+}
+</script>
+<style>
+
+</style>
---
+++ src/components/homeComps/HomeNoLogin.vue
@@ -0,0 +1,36 @@
+<template>
+       <b-card
+                  title="JWT Auth Demostration"
+                  img-src="https://picsum.photos/600/300/?image=25"
+                  img-alt="Image"
+                  img-top
+                  tag="article"
+                  style="max-width: 20rem;"
+                  class="mb-6 text-center display-flex"
+                  >
+                    <b-card-text>
+                    Samet's Homework
+                    </b-card-text>
+                    <b-row>
+                      <b-col xs="12"><b-button variant="warning"  v-on:click="routeRegister"> New Account</b-button></b-col>
+                    
+                      <b-col xs="12"><b-button variant="success"  v-on:click="routeLogin"> Login</b-button></b-col>
+                 </b-row>
+        </b-card>
+</template>
+<script>
+export default {
+    name:'homeNoLogin',
+    methods:{
+          routeRegister(){ 
+              this.$router.push('register');
+          },
+          routeLogin(){
+              this.$router.push('login');
+          }
+    }
+}
+</script>
+<style>
+
+</style>
